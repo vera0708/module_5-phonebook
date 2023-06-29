68da211e8866830755089a373ead450f01ad5c41
vera0708 <verasdee@gmail.com>
2023-06-29 20:46:47 +0700

    feat: made additional task lesson 8 module 5

--- phonebook/script/script.js
+++ phonebook/script/script.js
@@ -318,19 +318,48 @@ let data = [
         })
     };
 
+    const getSorted = (thname, list) => {
+        if (thname === 'Имя') {
+            let sortedRows = Array.from(list.rows).sort((rowA, rowB) => rowA.cells[1].innerHTML > rowB.cells[1].innerHTML ? 1 : -1);
+            list.append(...sortedRows);
+            // setStorageToItem('sort', thname)
+        }
+        if (thname === 'Фамилия') {
+            let sortedRows = Array.from(list.rows).sort((rowA, rowB) => rowA.cells[2].innerHTML > rowB.cells[2].innerHTML ? 1 : -1);
+            list.append(...sortedRows);
+        };
+        setStorageToItem('sort', thname);
+    };
+
     const sortRows = (thead, list) => {
         thead.addEventListener('click', e => {
             const target = e.target;
-            if (target.textContent === 'Имя') {
-                let sortedRows = Array.from(list.rows).sort((rowA, rowB) => rowA.cells[1].innerHTML > rowB.cells[1].innerHTML ? 1 : -1);
-                list.append(...sortedRows);
-            }
-            if (target.textContent === 'Фамилия') {
-                let sortedRows = Array.from(list.rows).sort((rowA, rowB) => rowA.cells[2].innerHTML > rowB.cells[2].innerHTML ? 1 : -1);
-                list.append(...sortedRows);
-            };
+            getSorted(target.textContent, list);
+            // if (target.textContent === 'Имя') {
+            //     let sortedRows = Array.from(list.rows).sort((rowA, rowB) => rowA.cells[1].innerHTML > rowB.cells[1].innerHTML ? 1 : -1);
+            //     list.append(...sortedRows);
+            // }
+            // if (target.textContent === 'Фамилия') {
+            //     let sortedRows = Array.from(list.rows).sort((rowA, rowB) => rowA.cells[2].innerHTML > rowB.cells[2].innerHTML ? 1 : -1);
+            //     list.append(...sortedRows);
+            // };
         });
     };
+
+    const makeSort = (list) => {
+        const sortThname = getStorageFromItem('sort');
+        if (sortThname) {
+            getSorted(sortThname, list);
+        }
+    };
+
+    const setStorageToItem = (key, value) => {
+        localStorage.setItem(key, value);
+    };
+
+    const getStorageFromItem = (key) => {
+        return localStorage.getItem(key);
+    }
 
     const getStorage = (key) => {
         if (localStorage.getItem(key)) {
@@ -360,6 +389,7 @@ let data = [
         formControl(form, list, closeModal);
 
         sortRows(thead, list);
+        makeSort(list);
 
         /* setTimeout(() => {
              const contact = createRow({
